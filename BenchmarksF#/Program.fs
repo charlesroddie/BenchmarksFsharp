fe61c753c1b11adeea8ff1441cefb7d53b779385
Charles Roddie <charles.roddie@summatic.co.uk>
2024-01-08 22:20:52 +0000

    Add project files.

---
+++ BenchmarksF#/Program.fs
@@ -0,0 +1,17 @@
+﻿module FSharpBenchmarks
+
+open System.Collections.Immutable
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+[<MemoryDiagnoser>]
+type Bench() =
+    
+
+    [<Benchmark>]
+    member _.Benchmark() = ()
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run<Bench>() |> ignore
+    0 // return an integer exit code
