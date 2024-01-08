5d08d62139961ffe17f5c684a2e5c7a412f27dd0
Charles Roddie <charles.roddie@summatic.co.uk>
2024-01-08 22:29:03 +0000

    string benchmarks

--- BenchmarksF#/Program.fs
+++ BenchmarksF#/Program.fs
@@ -1,12 +1,25 @@
-﻿module FSharpBenchmarks
+﻿module InterpolatedStringBenchmarks
 
+open System
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
+
+let world = "world"
 
 [<MemoryDiagnoser>]
 type Bench() =
     [<Benchmark>]
-    member _.Benchmark() = ()
+    member _.InterpolatedStringCurrent() = $"hello {world}"
+
+    [<Benchmark>]
+    member _.Impl1() = String.Format("hello {0}", world)
+
+    [<Benchmark>]
+    member _.Impl2() =
+        let sb = Text.StringBuilder()
+        sb.Append("hello") |> ignore
+        sb.Append(world) |> ignore
+        sb.ToString()
 
 [<EntryPoint>]
 let main argv =
